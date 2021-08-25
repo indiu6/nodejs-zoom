d2b0952875c4c163f45695e23bd184ae3351a9f4
Sean Yang <hewas6@gmail.com>
2021-08-24 23:56:23 -0400

    add pug, views

--- src/server.js
+++ src/server.js
@@ -1,7 +1,16 @@
 import express from 'express';
+import path from 'path';
+const __dirname = path.resolve();
 
 const app = express();
 
-console.log('hi');
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/src/views');
 
-app.listen(3000);
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.listen(3000, () => {
+  console.log('Listening on http://localhost:3000');
+});
