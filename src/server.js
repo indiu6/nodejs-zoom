7379d2f280b693f76d0a82a5925c05b9474b52b6
Sean Yang <hewas6@gmail.com>
2021-08-25 23:47:58 -0400

    add mvp-css

--- src/server.js
+++ src/server.js
@@ -7,6 +7,8 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/src/views');
 
+app.use('/public', express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
   res.render('home');
 });
